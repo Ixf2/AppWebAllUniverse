d61d562bc0c77857935548827eefed5433ca5c43
Joana <yopiixf@gmail.com>
2026-04-09 15:49:36 +0100

    Intento repositorio

---
+++ src/services/firebase/Firebase.jsx
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA4o0S_TAOZHbtJsJPIOoHFBWLrvTpmj_g",
+  authDomain: "appweballuniverse.firebaseapp.com",
+  projectId: "appweballuniverse",
+  storageBucket: "appweballuniverse.firebasestorage.app",
+  messagingSenderId: "395926709460",
+  appId: "1:395926709460:web:d8e5d0935eb830bcc38741"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
