4f3edfff6190ea5f96cb377129cc3e755dbd892d
sdvictorvergara <sdvictorvergara@gmail.com>
2026-04-13 19:24:32 +0100

    Añadida hamburguesa resposive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,20 +1,37 @@
+import { useState } from "react"
 import "./Header.css"
 import roundedLogo from '../../data/images/round-logo.svg'
+import hamburgerIcon from '../../data/images/Hamburger_icon.svg'
 
 function Header() {
+    const [menuOpen, setMenuOpen] = useState(false)
+
+    const toggleMenu = () => setMenuOpen(prev => !prev)
+    const closeMenu = () => setMenuOpen(false)
+
     return (
         <header>
             <a href="/home" id="header-logo">
-                <img  src={roundedLogo} alt="" />
+                <img src={roundedLogo} alt="" />
             </a>
-            <nav>
+
+            <nav className={menuOpen ? "nav-open" : ""}>
                 <ul>
-                    <li><a href="/home">Home</a></li>
-                    <li><a href="/elements">Elements</a></li>
-                    <li><a href="/missions">Missions</a></li>
-                    <li><a href="/about_us">About</a></li>
+                    <li><a href="/home" onClick={closeMenu}>Home</a></li>
+                    <li><a href="/elements" onClick={closeMenu}>Elements</a></li>
+                    <li><a href="/missions" onClick={closeMenu}>Missions</a></li>
+                    <li><a href="/about_us" onClick={closeMenu}>About</a></li>
                 </ul>
             </nav>
+
+            <button
+                id="hamburger-btn"
+                onClick={toggleMenu}
+                aria-label="Toggle menu"
+                aria-expanded={menuOpen}
+            >
+                <img src={hamburgerIcon} alt="Menu" />
+            </button>
         </header>
     )
 }
