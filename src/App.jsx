6b0b7d2e8cdcb90d181c379a7b7d0e44d8414d32
Joana <yopiixf@gmail.com>
2026-04-13 17:40:09 +0100

    Ruta APP actualizada

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Home from "./pages/home/Home";
 import Elements from "./pages/elements/Elements"
 import AboutUs from "./pages/aboutus/AboutUs"
-import ArtemisII from "./pages/artemisII/ArtemisII"
+import Missions from "./pages/missions/Missions"
 import Legal from "./pages/legal/Legal"
 import Terms from "./pages/legal/Terms"
 import NotFound from "./pages/notfound/Notfound"
@@ -15,7 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/elements" element={<Elements />} />
-        <Route path="/artemisII" element={<ArtemisII />} />
+        <Route path="/missions" element={<Missions />} />
         <Route path="/about_us" element={<AboutUs />} />
         <Route path="/legal" element={<Legal />} />
         <Route path="/terms" element={<Terms />} />
